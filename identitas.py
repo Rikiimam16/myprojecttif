6b7df59eff4ce4eb72e4464d87b680a401653319
Rikz Code <126227295+Rikiimam16@users.noreply.github.com>
2026-08-21 21:10:56 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,23 @@
+# Menampilkan Judul
+print("========================================================\n\t\tDATA DIRI MAHASISWA\n========================================================")
+
+# Menampilkan Nama Lengkap
+print("Nama\t\t\t: M. Riki Imamuddin")
+
+# Menampilkan NIM
+print("NIM\t\t\t: 2605060009")
+
+# Menampilkan Tempat, Tanggal Lahir
+print("Tempat, Tanggal Lahir\t: Pekalongan, 20 Maret 2008")
+
+# Menampilkan Alamat
+print("Alamat\t\t\t: Kab. Pekalongan, Jawa Tengah")
+
+# Menampilkan Jenis Kelamin
+print("Jenis Kelamin\t\t: Laki-laki")
+
+# Menampilkan Hobi
+print("Hobi\t\t\t: Mendengarkan Musik")
+
+# Menampilkan garis penutup
+print("========================================================")
